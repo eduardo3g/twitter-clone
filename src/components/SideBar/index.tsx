2dde975bb26ad092664f5a3d5f84991d7de07aff
eduardo3g <eduardosbrasil10@gmail.com>
2020-07-31 19:27:11 -0300

    Follow suggestion components.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
+
 import {
   Container,
   SearchWrapper,
@@ -17,7 +20,28 @@ const SideBar: React.FC = () => {
       </SearchWrapper>
 
       <Body>
-        <p>{'Lorem ipsum dolor sit amet. '.repeat(90)}</p>
+        <List 
+          title="Talvez você curta"
+          elements={[
+            // <FollowSuggestion
+            //   name="Guilherme Rodz"
+            //   nickname="@guilherme_rodz"
+            // />
+            <FollowSuggestion
+              name="Diego Fernandes"
+              nickname="@dieegosf"
+            />
+          ]}
+        />
+
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+            <h1>Test</h1>
+          ]}
+        />
       </Body>
     </Container>  
   );
